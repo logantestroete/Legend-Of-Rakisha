accde03e047783fc4b9ce9bd317a5802d67492c8
logantestroete <loganptestroete@gmail.com>
2026-08-19 19:15:16 -0500

    Added all of the images for weapons and armor

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92bd207ef9977e5dccfb8de38073ae630b0bbf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b373a03f10f65363bf3bcb008130153c7650079")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
